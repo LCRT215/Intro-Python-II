39a60f16215285f0a996765c2233382d73e02b06
Leslie Thompson <lcrthompson@gmail.com>
2020-08-10 09:49:48 -0400

    practice

--- store.py
+++ store.py
@@ -5,7 +5,7 @@ class Store:
     def __init__(self, name, categories):
         # init is like a constructor. It is used to define and construct all of the attributes
         # attributes
-        # self is like this in JS
+        # self is like this in JS. Always the first attribute
         self.name = name
         self.categories = categories
 
@@ -15,11 +15,13 @@ class Store:
         # 1. food
         # 2. clothes
         # 3. treats
+        # 4. Quit
         i = 1
         for category in self.categories:
-            output += f'\n {i}. {category}'
+            output += f'\n{i}. {category}'
             i += 1
-        return output   
+        output += f'\n{i}. Quit'
+        return output
 
     def __repr__(self):
         # __repr__ similar to __str__.
@@ -31,3 +33,15 @@ class Store:
 my_store = Store("Petapalooza", ["food", "clothes", "treats"])
 # print(my_store.__repr__())
 print(my_store)
+
+# add input parser
+selection = int(input("Please select a category number: \n")) - 1
+# print(f'you selected {my_store.categories[selection]}')
+
+while(selection != len(my_store.categories)):
+    print(f'you selected {my_store.categories[selection]}')
+    selection = int(input("Please select a category number: \n")) - 1
+     
+# using selection as the index to identify which category was chosen
+
+# Errors to check for: input is a valid number (greater than 0, less than len(categories))
